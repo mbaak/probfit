c83bffc2e4a4cad34762b4205d7e049d623a47ff
kx79wq <max.baak@ing.com>
2021-03-16 15:11:30 +0100

    weak labels pdf

---
+++ probfit/weaklabels.py
@@ -0,0 +1,371 @@
+import numpy as np
+from iminuit.util import make_func_code, describe
+from .funcutil import merge_func_code
+from .functor import construct_arg
+from scipy import special
+
+
+def vec_translate(a, my_dict):
+    return np.vectorize(my_dict.__getitem__)(a)
+
+
+def tuplize(x):
+    return tuple(x) if isinstance(x, (list, tuple, np.ndarray)) else tuple([] if x is None else [x])
+
+
+class Node(object):
+    def __init__(self, indices, states, cardinality, prefix='wl'):
+        """
+
+        :param indices:
+        """
+        # basic checks on inputs
+        if not isinstance(prefix, str) or len(prefix) == 0:
+            raise ValueError('prefix should be a filled string')
+        if not isinstance(cardinality, int) or cardinality < 0:
+            raise ValueError('cardinality should be a positive integer')
+        if isinstance(states, int) and states > 0:
+            states = list(range(states))
+        if not isinstance(states, (list, tuple, np.ndarray)) or len(states) == 0:
+            raise ValueError('states should be a filled list')
+        else:
+            states = list(states)
+        if not isinstance(indices, (list, tuple, np.ndarray)) or len(indices) == 0 or any([idx < 0 for idx in indices]):
+            raise ValueError('indices should be a filled list of indices')
+        else:
+            indices = np.array(indices)
+
+        self.prefix = prefix
+        self.indices = indices
+        indices_str = ''.join([str(idx) for idx in indices])
+        self.name = f'{prefix}{indices_str}'
+        self.states = states
+        self.n_states = len(states)
+        self.mapping = dict(zip(states, range(self.n_states)))
+        self.cardinality = cardinality
+        self.shape = (self.n_states - 1, self.cardinality)
+        self.n_independent = (self.n_states - 1) * self.cardinality
+        self.independent_probabilities = np.ones(self.n_independent)
+        self._update_conditional_probabilities()
+
+        # Leave out last probabilities, can also handle tuples of states
+        states_str = [[str(s) for s in tuplize(state)] for state in states[:-1]]
+        varnames = ['X'] + [f'{self.name}_{"_".join(state)}_{y}' for state in states_str for y in range(cardinality)]
+        self.func_code = make_func_code(varnames)
+        self.func_defaults = None
+
+    def _update_conditional_probabilities(self):
+        """Normalize the conditional probabilities
+        """
+        # negative entries to zero
+        cprobs = self.independent_probabilities.reshape(self.shape)
+        cprobs[cprobs < 0] = 0
+
+        # dependent probabilities are one minus the rest, and cannot be negative
+        dependent_probabilities = 1. - np.sum(cprobs, axis=0)
+        dependent_probabilities[dependent_probabilities < 0] = 0
+        dependent_probabilities = dependent_probabilities.reshape((1, -1))
+
+        # dependent probabilities inserted at last position of cond_probs matrix.
+        cprobs = np.concatenate([cprobs, dependent_probabilities], axis=0)
+
+        # normalize each column to one.
+        pnorm = cprobs.sum(axis=0)
+        if not np.all(pnorm > 0):
+            raise RuntimeError('conditional probabilities cannot be normalized')
+        self.conditional_probabilities = cprobs / pnorm[np.newaxis, :]
+
+    def __call__(self, *args):
+        X = args[0]
+        X = np.array([X] if not isinstance(X, (list, tuple, np.ndarray)) else X)
+        x = X[:, self.indices if len(self.indices) > 1 else self.indices[0]] if X.ndim > 1 else X
+        x_vec = vec_translate(x, self.mapping)
+
+        if len(args) == self.n_independent + 1:
+            self.independent_probabilities = np.array(args[1: self.n_independent + 1])
+            self._update_conditional_probabilities()
+        elif len(args) == 2:
+            iprobs = args[1]
+            if isinstance(iprobs, (list, tuple, np.ndarray)):
+                self.independent_probabilities = np.array(iprobs).ravel()
+                if len(self.independent_probabilities) != self.n_independent:
+                    raise ValueError(f'probabilities does not have {self.n_independent} entries')
+            self._update_conditional_probabilities()
+
+        # return selected conditional probabilities
+        return self.conditional_probabilities[x_vec if x_vec.shape[-1] > 1 else x_vec[0]]
+
+
+class WeakLabel(Node):
+    def __init__(self, index, states, cardinality, prefix='wl'):
+        if not isinstance(index, int) or index < 0:
+            raise ValueError('index should be a positive integer')
+        super(WeakLabel, self).__init__([index], states, cardinality, prefix)
+
+
+class CompoundWeakLabel(Node):
+    def __init__(self, weak_labels, prefix='cwl'):
+        if len(weak_labels) == 0 or not all([isinstance(wl, Node) for wl in weak_labels]):
+            raise TypeError('weak labels should be filled list of WeakLabels')
+
+        cardinalities = [wl.cardinality for wl in weak_labels]
+        if not cardinalities.count(cardinalities[0]) == len(cardinalities):
+            raise RuntimeError('weak labels should have the same cardinality')
+
+        indices = np.concatenate([wl.indices for wl in weak_labels])
+
+        def outer(A, B):
+            return [tuplize(x) + tuplize(y) for y in B for x in A]
+        states = [None]
+        for wl in weak_labels:
+            states = outer(states, wl.states)
+
+        super(CompoundWeakLabel, self).__init__(indices, states, cardinalities[0], prefix)
+
+
+class TrueLabel(object):
+    def __init__(self, cardinality, prefix='pY'):
+        # basic checks on inputs
+        if not isinstance(prefix, str) or len(prefix) == 0:
+            raise ValueError('prefix should be a filled string')
+        self.prefix = prefix
+        if not isinstance(cardinality, int) or cardinality < 0:
+            raise ValueError('cardinality should be a positive integer')
+        self.cardinality = cardinality
+
+        self.independent_probabilities = np.ones(self.cardinality - 1)
+        self._update_state_probabilities()
+
+        # set function code for bayesian model
+        varnames = [f'{self.prefix}_{y}' for y in range(self.cardinality - 1)]
+        self.func_code = make_func_code(varnames)
+
+    def _update_state_probabilities(self):
+        """Normalize the state probabilities
+        """
+        # negative entries to zero
+        cprobs = self.independent_probabilities
+        cprobs[cprobs < 0] = 0
+
+        # dependent probability is one minus the rest, and cannot be negative
+        dependent_probability = 1. - np.sum(cprobs)
+        dependent_probability = 0 if dependent_probability < 0 else dependent_probability
+
+        # dependent probability inserted at last position of states.
+        cprobs = np.concatenate([cprobs, [dependent_probability]])
+
+        # normalize sum to 1
+        pnorm = cprobs.sum()
+        if pnorm <= 0:
+            raise RuntimeError('state probabilities cannot be normalized')
+        self.state_probabilities = cprobs / pnorm
+
+    def __call__(self, *args):
+        if len(args) == self.cardinality - 1:
+            self.independent_probabilities = np.array(args[0: self.cardinality - 1])
+            self._update_state_probabilities()
+        elif len(args) == 1:
+            iprobs = args[1]
+            if isinstance(iprobs, (list, tuple, np.ndarray)):
+                self.independent_probabilities = np.array(iprobs).ravel()
+                if len(self.independent_probabilities) != self.cardinality - 1:
+                    raise ValueError(f'independent probabilities does not have {self.cardinality - 1} entries')
+            self._update_state_probabilities()
+
+        # return all state probabilities
+        return self.state_probabilities
+
+
+class BayesianModel(object):
+    def __init__(self, weak_labels, prefix="pY"):
+        # basic checks on inputs
+        if not isinstance(prefix, str) or len(prefix) == 0:
+            raise ValueError('prefix should be a filled string')
+        if len(weak_labels) == 0 or not all([isinstance(c, Node) for c in weak_labels]):
+            raise TypeError('weak_labels attribute should be filled list of weak_labels')
+
+        indices = np.concatenate([c.indices for c in weak_labels])
+        u_indices = np.unique(indices)
+        if len(indices) != len(u_indices):
+            raise RuntimeError('overlapping indices between weak_labels. weak_labels are not independent.')
+
+        cardinalities = [wl.cardinality for wl in weak_labels]
+        self.cardinality = cardinalities[0]
+        if not cardinalities.count(self.cardinality) == len(cardinalities):
+            raise RuntimeError('weak_labels and true_label should all have the same cardinality')
+        self.classes = np.array(list(range(self.cardinality)))
+
+        # define true label (class imbalance parameters)
+        true_label = TrueLabel(self.cardinality, prefix)
+
+        # set function code
+        arg = tuple(weak_labels) + (true_label, )
+        self.func_code, allpos = merge_func_code(*arg)
+
+        # store functions & caching
+        self.allf = arg  # f function
+        self.allpos = allpos  # position for f arg
+        self.numf = len(self.allf)
+        self.arglen = self.func_code.co_argcount
+        self.min_pdf_value = 1e-300
+
+    def __call__(self, *arg):
+        return self.probability(*arg)
+
+    def _eval(self, *arg):
+        # probabilities of the true label states
+        if self.arglen != len(arg) and len(arg) != 1:
+            raise AssertionError('wrong number of arguments')
+
+        # multiply probabilities all weak labels and the underlying true state
+        yprobs = np.ones(self.cardinality)
+        for i in range(self.numf):
+            thispos = self.allpos[i]
+            if self.arglen == len(arg):
+                this_arg = construct_arg(arg, thispos)
+            elif len(arg) == 1 and i != self.numf - 1:
+                # first argument iS X, needed by all weak labels
+                this_arg = arg
+            else:  # len(arg) == 1
+                # true_label, does not take X
+                this_arg = ()
+            tmp = self.allf[i](*this_arg)
+            yprobs = yprobs * tmp
+
+        return yprobs
+
+    def probability(self, *arg):
+        """Return probability estimates for the test vector X.
+
+        :param X: array-like of shape (n_samples, n_features)
+        :return: array-like of shape (n_samples, n_classes)
+            Returns the log-probability of the samples for each class in
+            the model. The columns correspond to the classes in sorted
+            order, as they appear in the attribute :term:`classes_`.
+        """
+        yprobs = self._eval(*arg)
+
+        # sum probabilities over the true label states
+        probs = yprobs.sum(axis=1) if len(yprobs.shape) == 2 else yprobs.sum()
+
+        # prevent probability values of zero. if so return tiny value instead
+        is_array = isinstance(probs, np.ndarray)
+        probs = np.array(probs if is_array else [probs])
+        probs[probs < self.min_pdf_value] = self.min_pdf_value
+        return probs if is_array else probs[0]
+
+    def log_probability(self, *arg):
+        """Return log-probability estimates for the test vector X.
+
+        :param X: array-like of shape (n_samples, n_features)
+        :return: array-like of shape (n_samples, n_classes)
+            Returns the log-probability of the samples for each class in
+            the model. The columns correspond to the classes in sorted
+            order, as they appear in the attribute :term:`classes_`.
+        """
+        return np.log(self.probability(*arg))
+
+    def predict(self, *arg):
+        """Perform classification on an array of test vectors X.
+
+        :param X: array-like of shape (n_samples, n_features)
+        :return: ndarray of shape (n_samples,)
+            Predicted target values for X
+        """
+        prob_class = self.predict_proba(*arg)
+        return self.classes[np.argmax(prob_class, axis=1)]
+
+    def predict_log_proba(self, *arg):
+        """Return log-probability estimates for the test vector X.
+
+        :param X: array-like of shape (n_samples, n_features)
+        :return: array-like of shape (n_samples, n_classes)
+            Returns the log-probability of the samples for each class in
+            the model. The columns correspond to the classes in sorted
+            order, as they appear in the attribute :term:`classes_`.
+        """
+        return np.log(self.predict_proba(*arg))
+
+    def predict_proba(self, *arg):
+        """ Return probability estimates for the test vector X.
+
+        :param X: array-like of shape (n_samples, n_features)
+        :return: array-like of shape (n_samples, n_classes)
+            Returns the probability of the samples for each class in
+            the model. The columns correspond to the classes in sorted
+            order, as they appear in the attribute :term:`classes_`.
+        """
+        # sum probabilities over the true label states
+        prob_class = self._eval(*arg)
+        prob_sum = prob_class.sum(axis=1) if len(prob_class.shape) == 2 else prob_class.sum()
+
+        # prevent probability values of zero. if so return tiny value instead
+        is_array = isinstance(prob_sum, np.ndarray)
+        sum_zero_check = np.array(prob_sum if is_array else [prob_sum])
+        zero_mask = sum_zero_check < self.min_pdf_value
+        if np.any(zero_mask):
+            # need to redo prob_sum
+            prob_class[zero_mask] = self.min_pdf_value
+            prob_sum = prob_class.sum(axis=1) if len(prob_class.shape) == 2 else prob_class.sum()
+
+        # per class probability
+        proba = prob_class / (prob_sum[:, np.newaxis] if is_array else prob_sum)
+        return proba
+
+
+class BinnedLabels(object):
+    def __init__(self, X, select_indices=None):
+        """"""
+        # basic checks on inputs
+        if not isinstance(X, (list, tuple, np.ndarray)):
+            raise TypeError('X should be a filled numpy array')
+        X = np.array(X)
+
+        if isinstance(select_indices, (list, tuple, np.ndarray)):
+            select_indices = np.array(select_indices)
+            if len(select_indices) == 0 or any([idx < 0 for idx in select_indices]):
+                raise ValueError('select_indices should be a filled list of valid indices')
+            X = X[:, select_indices if len(select_indices) > 1 else select_indices[0]] if X.ndim > 1 else X
+
+        # make binned dataset with counts per bin
+        # we don't store empty bins to save memory.
+        self.bin_x, self.bin_entries = np.unique(X, return_counts=True, axis=0)
+        self.n_samples = np.sum(self.bin_entries)
+        # dict only takes tuples as keys
+        self.data = dict(zip([tuple(x) for x in self.bin_x], self.bin_entries))
+
+
+class BinnedLH(object):
+    def __init__(self, binned_data, bayes_model):
+        assert isinstance(binned_data, BinnedLabels)
+        assert isinstance(bayes_model, BayesianModel)
+
+        self.data = binned_data
+        self.bm = bayes_model
+
+        # skip X! that will be binned_data
+        varnames = describe(bayes_model)[1:]
+        self.func_code = make_func_code(varnames)
+        self.arglen = len(varnames)
+        self.parameter_names = varnames
+
+    def __call__(self, *arg):
+        if self.arglen != len(arg):
+            raise AssertionError('wrong number of arguments')
+
+        x_arg = (self.data.bin_x, ) + arg
+        probs = self.bm(*x_arg)
+
+        # predicted number of entries per bin, for all filled bins
+        f = self.data.n_samples * probs
+        # predicted number of entries for bins with zero entries
+        f_zero = self.data.n_samples * (1. - np.sum(probs))
+
+        # observed number of entries
+        y = self.data.bin_entries
+
+        # Poisson binned likelihood for filled bins
+        nll = f - special.xlogy(y, f) + special.gammaln(y + 1)
+
+        # likelihood sum of filled and unfilled bins
+        return np.sum(nll) + f_zero
